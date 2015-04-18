47d6bd753ad78d3c0edbafecc1e1b33482a8227d
wpegg-dev <william.pegg.dev@gmail.com>
2015-04-18 10:58:19 -0400

    Added Forecast
    Added a 7 day forecast to widget

--- WeatherWidget/app/controllers/main_controller.rb
+++ WeatherWidget/app/controllers/main_controller.rb
@@ -37,5 +37,47 @@ class MainController < ApplicationController
     when "tornado" 
       @main.currently.icon = "wi wi-tornado"
     end
+    
+    weekHash = @main.daily
+    
+    weekHash.each do |key, value|
+      curItem = weekHash[key]
+      @daily = weekHash.to_a
+    end
+    
+    tmpArry = @daily[2][1]
+    tmpArry.each do |elem|
+      cond = elem.icon
+      case cond
+      when "clear-day" 
+        elem.icon = "wi wi-day-sunny"
+      when "clear-night"
+        elem.icon = "wi wi-night-clear"
+      when "rain"
+        elem.icon = "wi wi-showers"
+      when "snow" 
+        elem.icon = "wi wi-snow"
+      when "sleet" 
+        elem.icon = "wi wi-sleet"  
+      when "wind"
+        elem.icon = "wi wi-cloudy-windy"  
+      when "fog " 
+        elem.icon = "wi wi-fog"
+      when "cloudy" 
+        elem.icon = "wi wi-cloudy"  
+      when "partly-cloudy-day" 
+        elem.icon = "wi wi-day-sunny-overcast"
+      when "partly-cloudy-night" 
+        elem.icon = "wi wi-night-cloudy"
+      when "hail" 
+        elem.icon = "wi wi-hail"
+      when "thunderstorm" 
+        elem.icon = "wi wi-thunderstorm"
+      when "tornado" 
+        elem.icon = "wi wi-tornado"
+      end
+    end
+    
   end
+    
 end
