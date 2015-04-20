770f47d444afcb9ffdb947efb8c33789ef8ab790
wpegg-dev <william.pegg.dev@gmail.com>
2015-04-20 12:59:29 -0400

    Uploaded changes to Playback App

--- PlaybackWidget/app/assets/javascripts/application.js
+++ PlaybackWidget/app/assets/javascripts/application.js
@@ -15,31 +15,61 @@
 //= require turbolinks
 //= require_tree .
 $(document).ready(function(){  
-	//$( "#restartDialog" ).dialog({
-	//    autoOpen: false
-	//  });
 	
-	var sound = new Audio('/assets/Uptown_Funk.m4a');
+	var ca = document.cookie.split(';');
+	var song = '';
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf("song=") == 0) song = c.substring("song=".length, c.length);
+    }
+    
+    var sound = '';
+    var pgRealod = false;
+    if(song != "" && song != null){
+    	sound = new Audio('/assets/'+song);
+    	$("#selectSongs").val(song);
+    	pgReload = true;
+    }
+    else{
+    	sound = new Audio('/assets/Uptown_Funk.m4a');
+    }
+	
+	$('#selectSongs').addClass("btn btn-warning dropdown-toggle");
 	
 	sound
 		.isPlayed = false
 		.volume = 1;
 	
-	
-	$('#butt').on('click',function(){
-		//alert("button clicked");
-		if (!sound.isPlayed) {
-			sound.play(); // Play sound
-			$(this)
-				.removeClass('btn-warning glyphicon-play')//.removeClass('btn-succes glyphicon-play')
-				.addClass('btn-info glyphicon-pause');//.addClass('btn-danger glyphicon-pause');
-		} else {
-			sound.pause(); // Pause sound
-			$(this)
-				.removeClass('btn-info glyphicon-pause')//.removeClass('btn-danger glyphicon-pause')
-				.addClass('btn-warning glyphicon-play');//.addClass('btn-success glyphicon-play');
+	/*TODO: need to look into why the button needs to be clicked twice to work*/
+	$('#butt').on('click',function(e){
+		
+		if($("#selectSongs").val() === "Select a Song"){
+			alert("Please Select a song.");		
+		}else{
+			var userOption = $("#selectSongs").val();
+			if(userOption != song){
+				var date = new Date();
+			    date.setTime(date.getTime() + (300000));
+			    var expires = "; expires=" + date.toGMTString();
+				document.cookie = 'song='+$("#selectSongs").val()+'; expires='+expires;
+				window.location.reload(true);
+			} else {
+				if (!sound.isPlayed) {
+					sound.play(); // Play sound
+					$(this)
+						.removeClass('btn-warning glyphicon-play')
+						.addClass('btn-info glyphicon-pause');
+				} else {
+					sound.pause(); // Pause sound
+					$(this)
+						.removeClass('btn-info glyphicon-pause')
+						.addClass('btn-warning glyphicon-play');
+				}
+				sound.isPlayed = !sound.isPlayed;	
+			}
 		}
-		sound.isPlayed = !sound.isPlayed;
+		
 	});
 	
 	$('#vol-value').css({'width': sound.volume * 100 + "%"});
--- PlaybackWidget/app/controllers/playback_controller_controller.rb
+++ PlaybackWidget/app/controllers/playback_controller_controller.rb
@@ -1,6 +1,7 @@
-
 class PlaybackControllerController < ApplicationController
    def index
-
+    @songs = Dir.entries("#{Rails.root}/app/assets/audios")
+    @songs.delete("..")
+    @songs[0] = "Select a Song"
    end
 end
